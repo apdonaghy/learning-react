60168736171840fd900d6dd1194e5a8c8aca7e2a
apdonaghy <adonaghy@seattleu.edu>
2021-04-22 19:46:09 -0700

    learning about state in react

---
+++ seasons/src/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+class App extends React.Component {
+
+        state = { lat: null, errorMessage: '' };
+
+        componentDidMount(){
+            // data loading in here, API JSON
+            window.navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    this.setState({ lat: position.coords.latitude})
+                },
+                (err) => {
+                    this.setState({ errorMessage: err.message })
+                }
+            );
+    }
+    
+ 
+
+    componentDidUpdate(){
+        console.log('component did update')
+    }
+
+    render() {
+      
+            if(this.state.errorMessage && !this.state.lat){
+                return <div>return Error: {this.state.errorMessage}</div>
+            }
+          
+            else if(!this.state.errorMessage && this.state.lat){
+                return <div>latitude: {this.state.lat}</div>
+            }else{
+            return <div>Loading</div>
+            }
+    }
+}
+
+ReactDOM.render( <
+    App / > ,
+    document.querySelector('#root')
+)
