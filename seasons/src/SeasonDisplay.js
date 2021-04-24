a9541d22c777614c8522ec58fe0e46d3e42b38eb
apdonaghy <adonaghy@seattleu.edu>
2021-04-23 17:17:30 -0700

    refactoring

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+    summer:{
+        text: `It's time to go to the beach`,
+        iconName: 'sun'
+    },
+    winter:{
+        text: `It's cold`,
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = function(lat, month){
+    if(month > 2 && month < 9){
+        // ternary expression
+       return lat > 0 ? 'summer' : 'winter';
+    }else{
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth())
+    const {text, iconName} = seasonConfig[season]
+    
     return(
-        <div>season display</div>
+        <div>
+            <i className={`${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`${iconName} icon`} />
+        </div>
     )
 };
 
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import SeasonDisplay from "./SeasonDisplay"
 
 class App extends React.Component {
 
@@ -31,7 +31,8 @@ class App extends React.Component {
             }
           
             else if(!this.state.errorMessage && this.state.lat){
-                return <div>latitude: {this.state.lat}</div>
+                // return <div>latitude: {this.state.lat}</div>
+                return <SeasonDisplay lat={this.state.lat} />
             }else{
             return <div>Loading</div>
             }
