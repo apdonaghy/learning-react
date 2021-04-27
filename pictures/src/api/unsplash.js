77d1197e47444ae327f227af15a0ae7f9a4cb2dc
apdonaghy <adonaghy@seattleu.edu>
2021-04-26 21:27:19 -0700

    testing git ignore

--- pictures/src/api/unsplash.js
+++ pictures/src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.unsplash.com/',
     headers:{
-        Authorization: 'Client-ID JNmiknyRA49DznVn8GPBfLRnx6nJ5qSB6QKU-QeANs0'
+        // Authorization: 'Client-ID JNmiknyRA49DznVn8GPBfLRnx6nJ5qSB6QKU-QeANs0'
     }
 });
 
